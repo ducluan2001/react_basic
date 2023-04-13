31a935a16706e790c213725aa00d4151486f2faf
ducluan2001 <ndluan1212@gmail.com>
2023-04-14 01:03:34 +0700

    1

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 // tao class component bat dau bang chu in hoa
 class MyComponent extends React.Component {
+    state = {
+        name: 'đức luận',
+        age: 22,
+    };
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    };
+
     render() {
-        let name = 'duc luan';
-        return <div>hello my name {name}</div>;
+        return (
+            <>
+                <div>
+                    <input type="text" value={this.state.name} onChange={(event) => this.handleOnChangeName(event)} />{' '}
+                    <br />
+                    hello my name {this.state.name}
+                </div>
+                <div>i am {this.state.age} year old</div>
+            </>
+        );
     }
 }
 // xuat ra
